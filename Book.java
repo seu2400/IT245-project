d8ab8151d5a5d7c8744f401cfc62b66335d3e9b3
seu2400 <s240012490@seu.edu.sa>
2026-08-06 21:52:05 +0300

    Add constructor to Book class

--- Book.java
+++ Book.java
@@ -6,4 +6,13 @@ public class Book {
   private String genre;
   private int publicationYear;
 
+ public Book(String isbn, String title, String author,
+            String genre, int publicationYear) {
+
+    this.isbn = isbn;
+    this.title = title;
+    this.author = author;
+    this.genre = genre;
+    this.publicationYear = publicationYear;
+} 
 }
