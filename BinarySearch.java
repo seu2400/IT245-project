c7396bde6263ab0f988d944b7e96f5a898ca6a3a
seu2400 <s240012490@seu.edu.sa>
2026-08-06 22:29:44 +0300

    Added ISBN comparison
    Add comparison for ISBN in binary search

--- BinarySearch.java
+++ BinarySearch.java
@@ -9,6 +9,11 @@ public class BinarySearch {
     while (left <= right) {
 
         int middle = (left + right) / 2;
+        int comparison = books[middle].getIsbn().compareTo(isbn);
+
+        if (comparison == 0) {
+            return books[middle];
+}
 
         return null;
 }
