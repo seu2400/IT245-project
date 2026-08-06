e02f13aa735471a8eaa1df00d1edc91736ce014a
seu2400 <s240012490@seu.edu.sa>
2026-08-06 22:23:25 +0300

    Initialize search boundaries

--- BinarySearch.java
+++ BinarySearch.java
@@ -3,6 +3,8 @@ public class BinarySearch {
 
     public static Book binarySearch(Book[] books, String isbn) {
 
+        int left = 0
+        int right = books.length - 1;
         return null;
     }
 
